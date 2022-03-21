3190958a72759470f7a1f5e3851ad1ac26ea767d
phoenix52 <phoenix@tape.ooo>
2022-03-21 13:37:42 +0900

    [ADD] Vuetify with Todo App

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,28 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Todo from "../views/Todo.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "Todo",
+    component: Todo,
   },
   {
-    path: '/about',
-    name: 'about',
+    path: "/about",
+    name: "about",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+];
 
 const router = new VueRouter({
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere nisi
+      aperiam cupiditate sit, doloribus ab quam molestias impedit! Perferendis
+      itaque ab totam architecto iure minima natus iusto illo esse explicabo!
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere nisi
+      aperiam cupiditate sit, doloribus ab quam molestias impedit! Perferendis
+      itaque ab totam architecto iure minima natus iusto illo esse explicabo!
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere nisi
+      aperiam cupiditate sit, doloribus ab quam molestias impedit! Perferendis
+      itaque ab totam architecto iure minima natus iusto illo esse explicabo!
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere nisi
+      aperiam cupiditate sit, doloribus ab quam molestias impedit! Perferendis
+      itaque ab totam architecto iure minima natus iusto illo esse explicabo!
+    </p>
   </div>
 </template>
